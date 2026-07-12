4147542aa9c62d0f232c1324df95d860affa5c81
Ömer Uzunsoy <uzunsoyomer@gmail.com>
2026-07-12 10:58:48 +0300

    Sürüm notları güncellemsi

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "com.omeruzunsoy.uzunsiptv"
         minSdk = 24
         targetSdk = 35
-        versionCode = 19
-        versionName = "2.0"
+        versionCode = 20
+        versionName = "2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         lint {
